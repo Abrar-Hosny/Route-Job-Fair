41ff274500442e72206bded62e42f65d919045ed
Abrar Hosny <109631053+Abrar-Hosny@users.noreply.github.com>
2024-07-12 23:12:20 +0300

    first commit

---
+++ Route-Job-Fair/src/components/Button.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Button = () => {
+  return(
+<div>
+    <button className="btn btn-danger">hrllo</button>
+</div>
+  )
+
+};
+
+export default Button;
---
+++ Route-Job-Fair/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav class="navbar bg-body-tertiary">
+      <div class="container-fluid">
+        <span class="navbar-brand mb-0 h1">Route Job Fair</span>
+      </div>
+    </nav>  )
+}
+
+export default Navbar
